aeeda785a863d2438a77d5aa336bcffbef848ba7
Gosuhan <gosuhan@hotmail.fr>
2017-11-15 12:17:34 +0100

    test

---
+++ js/myjs.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+$(document).ready(function() {
+    $('select').material_select();
+  });
+
+$('select').material_select('destroy');
